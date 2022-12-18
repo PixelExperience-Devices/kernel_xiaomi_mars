arter97/android_kernel_oneplus_sm8350

Makefile: setup toolchain
Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

Makefile
@@ -361,7 +361,9 @@ include scripts/subarch.include
 # Alternatively CROSS_COMPILE can be set in the environment.
 # Default value for CROSS_COMPILE is not to prefix executables
 # Note: Some architectures assign CROSS_COMPILE in their arch/*/Makefile
-ARCH		?= $(SUBARCH)
+override ARCH		:= arm64
+override CROSS_COMPILE	:= /home/arter97/arm64-gcc/bin/aarch64-elf-
+override CROSS_COMPILE_ARM32	:= /home/arter97/arm32-gcc/bin/arm-eabi-
 
 # Architecture as present in compile.h
 UTS_MACHINE 	:= $(ARCH)

PixelExperience-Devices/kernel_xiaomi_mars

Signed-off-by: Flower Sea <zzp06208@gmail.com>

Makefile
@@ -361,7 +361,9 @@ include scripts/subarch.include
 # Alternatively CROSS_COMPILE can be set in the environment.
 # Default value for CROSS_COMPILE is not to prefix executables
 # Note: Some architectures assign CROSS_COMPILE in their arch/*/Makefile
-ARCH		?= $(SUBARCH)
+override ARCH		:= arm64
+override CROSS_COMPILE	:= /home/FlowerSea/arm64-gcc/bin/aarch64-elf-
+override CROSS_COMPILE_ARM32	:= /home/FlowerSea/arm32-gcc/bin/arm-eabi-
 
 # Architecture as present in compile.h
 UTS_MACHINE 	:= $(ARCH)