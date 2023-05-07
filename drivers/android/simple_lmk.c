0e00d849d7e60aa6b1fdb83d9ba6aa2144d8433a
Sultan Alsawaf <sultan@kerneltoast.com>
2023-05-07 11:25:37 -0700

    simple_lmk: Reduce unnecessary wake ups
    We can check if the waitqueue is actually active before calling wake_up()
    in order to avoid an unnecessary wake_up() if the reclaim thread is already
    running. Furthermore, the release barrier when zeroing needs_reclaim is
    unnecessary, so remove it.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -305,7 +305,7 @@ static int simple_lmk_reclaim_thread(void *data)
 	while (1) {
 		wait_event_freezable(oom_waitq, atomic_read(&needs_reclaim));
 		scan_and_kill();
-		atomic_set_release(&needs_reclaim, 0);
+		atomic_set(&needs_reclaim, 0);
 	}
 
 	return 0;
@@ -332,7 +332,9 @@ void simple_lmk_mm_freed(struct mm_struct *mm)
 
 void simple_lmk_trigger(void)
 {
-	if (!atomic_cmpxchg_acquire(&needs_reclaim, 0, 1))
+	atomic_set(&needs_reclaim, 1);
+	smp_mb__after_atomic();
+	if (waitqueue_active(&oom_waitq))
 		wake_up(&oom_waitq);
 }
 
